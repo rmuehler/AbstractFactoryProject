f4c2ae7f825dbd12df318acd6c1a7d2b32b01d65
kevinhuynh269 <kevinhuynh269@yahoo.com>
2021-05-14 14:32:08 -0500

    Printed Out the factory and got an input from the user so that they can see the menus

--- AbstractFactoryApp/Program.cs
+++ AbstractFactoryApp/Program.cs
@@ -6,9 +6,22 @@ namespace AbstractFactoryApp
     {
         private static void Main(string[] args)
         {
-            Console.Out.WriteLine("What type of food do you want?: ");
+            Console.Out.WriteLine("What type of food do you want?: Italian/French");
             string input = Console.ReadLine();
             var factory = GetFactory.Get(input);
+            var appitizer = factory.GenerateAppetizerMenu();
+            var main = factory.GenerateMainMenu();
+            var drink = factory.GenerateDrinkMenu();
+            var dessert = factory.GenerateDessertMenu();
+            
+            Console.WriteLine("Appetizer Menu:");
+            appitizer.PrintMenu();
+            Console.WriteLine("Main Menu");
+            main.PrintMenu();
+            Console.Out.WriteLine("Drink Menu");
+            drink.PrintMenu();
+            Console.Out.WriteLine("Dessert Menu");
+            dessert.PrintMenu();
         }
     }
 }
